d05589467e7fea334209877e60d96631435fe2e2
matsumoto <44424144+mgen3104@users.noreply.github.com>
2018-11-14 21:00:56 +0900

    少し編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
-  def edit
-  end
-
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+  def show
+    @user = User.find(params[:id])
+    @name = current_user.name
+    @posts = @user.posts.order("updated_at DESC")
   end
 
   private
